7205d633140f11bf871381a8352254409aabd7a7
rlebre <ruilebre@ua.pt>
2017-09-15 11:56:12 +0100

    added icon and logo created dashboard start page

--- src/app/sidebar/selected-entity/selected-entity.service.ts
+++ src/app/sidebar/selected-entity/selected-entity.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SelectedEntityService {
 
-  selectedSection: String;
+  selectedSection = 'dashboard';
 
   constructor() { }
 
