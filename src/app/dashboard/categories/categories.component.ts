4112ee5eaec9c32355d629f07cf5d152e56ac136
rlebre <ruilebre@ua.pt>
2017-09-18 17:31:17 +0100

    added crud for category entity fixed missed services on app.module

--- src/app/dashboard/categories/categories.component.ts
+++ src/app/dashboard/categories/categories.component.ts
@@ -1,15 +1,124 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Modal, BSModalContext } from 'ngx-modialog/plugins/bootstrap';
+import { Overlay, overlayConfigFactory } from 'ngx-modialog';
+import { ModalNewEntity, InputField } from '../modal-new-entity/modal-new-entity.component';
+
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { CategoriesService } from './categories.service';
+
+import { Observable } from 'rxjs/Rx';
+import { FormGroup } from '@angular/forms';
+
+
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+  fields: InputField[];
+  dataAvailable: boolean;
+  tableData: any;
+  isRefreshing = false;
+  timer;
+  apiEndpoint: String;
+  idToDelete: Number;
+  showDialogDeleteConfirmation = false;
+  showDialogErrorGettingAllCategories = false;
+  errorMessage: String;
+  caller: String;
 
-  constructor() { }
+  constructor(public fb: FormBuilder, public modal: Modal, private categoriesService: CategoriesService) {
+    let name = new InputField("name", "Name", "text");
 
-  ngOnInit() {
+    this.fields = [name];
+    this.apiEndpoint = "categories";
+    this.caller = "Category";
   }
 
+  ngOnInit() {
+    this.requestAllCategoriesFromServer();
+  }
+
+
+  openNewObjectModalWindow() {
+    let newObjectForm = this.fb.group({
+      name: ["", Validators.compose([Validators.minLength(3), Validators.required])]
+    });
+
+    const dialogRef = this.modal.open(ModalNewEntity,
+      overlayConfigFactory({ caller: this.caller, apiName: this.apiEndpoint, fields: this.fields, formGroup: newObjectForm }, BSModalContext));
+    dialogRef
+      .then(dialogRef => {
+        dialogRef.result.then(result => this.refresh());
+      });
+  }
+
+  fillTableWithReceivedData(jsonList) {
+    if (jsonList.length === 0) {
+      this.dataAvailable = false;
+      return;
+    }
+
+    this.tableData = jsonList;
+    this.dataAvailable = true;
+  }
+
+
+  refresh() {
+    this.isRefreshing = true;
+    this.timer = Observable.timer(1000);
+    this.timer.subscribe(t => this.tickerFunc(t));
+
+    this.requestAllCategoriesFromServer();
+  }
+
+  tickerFunc(tick) {
+    this.isRefreshing = false;
+  }
+
+  requestAllCategoriesFromServer() {
+    this.categoriesService.getAll(this.apiEndpoint).subscribe(
+      response => {
+        this.fillTableWithReceivedData(response.json());
+      },
+      err => {
+        this.errorMessage = err.status + " - " + err.statusText;
+        this.showDialogErrorGettingAllCategories = true;
+      });
+  }
+
+  editObject(category: any) {
+    var form = this.fb.group({
+      name: [category.name, Validators.required],
+    })
+
+    const dialogRef = this.modal.open(ModalNewEntity, overlayConfigFactory({ caller: this.caller, apiName: this.apiEndpoint, fields: this.fields, formGroup: form, aa: this.refresh }, BSModalContext));
+    dialogRef
+      .then(dialogRef => {
+        dialogRef.result.then(result => this.refresh());
+      });
+  }
+
+  deleteObject(category: any) {
+    this.showDialogDeleteConfirmation = true;
+    this.idToDelete = category.id;
+  }
+
+  showDialogErrorCloseClick() {
+    this.showDialogErrorGettingAllCategories = false;
+  }
+
+  deleteConfirmationYes() {
+    this.categoriesService.delete(this.apiEndpoint, this.idToDelete).subscribe(response => {
+      this.refresh();
+      this.showDialogDeleteConfirmation = false;
+    });
+  }
+
+  deleteConfirmationNo() {
+    this.showDialogDeleteConfirmation = false;
+  }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,16 +7,6 @@ import { SelectedEntityService } from '../sidebar/selected-entity/selected-entit
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  title = 'app';
-
-  private _opened = true;
-
-  private _toggleSidebar() {
-    this._opened = !this._opened;
-    console.log(this._opened);
-  }
-
   constructor(private selectedEntityService: SelectedEntityService) { }
 
   ngOnInit() {
